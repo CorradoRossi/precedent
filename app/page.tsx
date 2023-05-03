67149d7b6d439a1b4c7c422a932564182364822b
Rossi Meacham <rossi@frugaltricks.com>
2023-05-02 22:45:04 -0400

    first changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
   const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
+    "https://api.github.com/repos/corradorossi/precedent",
     {
       ...(process.env.GITHUB_OAUTH_TOKEN && {
         headers: {
@@ -81,7 +81,7 @@ export default async function Home() {
           </a>
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/steven-tey/precedent"
+            href="https://github.com/corradorossi/precedent"
             target="_blank"
             rel="noopener noreferrer"
           >
--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: "/github",
-        destination: "https://github.com/steven-tey/precedent",
+        destination: "https://github.com/corradorossi/precedent",
         permanent: false,
       },
     ];
